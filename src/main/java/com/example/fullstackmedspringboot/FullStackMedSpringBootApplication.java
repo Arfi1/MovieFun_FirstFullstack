b4f95c626a411960f1a7e2db43515448608eb016
arfi <arma0001@stud.kea.dk>
2024-03-20 13:42:37 +0100

    Initial commit

---
+++ src/main/java/com/example/fullstackmedspringboot/FullStackMedSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.fullstackmedspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackMedSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullStackMedSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fullstackmedspringboot/FullStackMedSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fullstackmedspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackMedSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
